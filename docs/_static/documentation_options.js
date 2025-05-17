b6b3b1f3ee68d51fb1d221e047e498ccba0661b5
reeduxx <brighton.ulery@gmail.com>
2025-05-17 00:23:37 -0500

    Define some new UI constants, versioning

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.1-a',
+    VERSION: '0.1.0-a',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src'))
+from common import __version__
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -13,7 +14,8 @@ sys.path.insert(0, os.path.abspath('../../src'))
 project = 'Into the Depths'
 copyright = '2025, Brighton Ulery and Caleb Buth'
 author = 'Brighton Ulery and Caleb Buth'
-release = '0.0.1-a'
+release = __version__
+version = __version__.split('-')[0]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
