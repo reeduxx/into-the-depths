2dc080e055d448ce51b61f5454789149c481f96a
reeduxx <brighton.ulery@gmail.com>
2025-05-19 09:21:49 -0500

    Begin refactoring menus

--- src/client/constants.py
+++ src/client/constants.py
@@ -2,3 +2,7 @@ BLOCK_SHADING = {
     "opaque": {'█', '▌', '▀', '▄'},
     "transparent": {'▓', '▒', '░'}
 }
+
+# TODO: Determine optimal minimum window size
+# MIN_WIDTH = 720
+# MIN_HEIGHT = 480
---
+++ src/common/__init__.py
@@ -0,0 +1,2 @@
+# MAJOR.MINOR.PATCH[-pre-release-tag]
+__version__ = "0.1.0-a"
